40be39ff965f73f5dbb7e3e8418269d4d8417940
Choms <choms@botmania.net>
2012-10-15 14:02:09 +0200

    Distribution recognition (apt/yum)

--- src/server.rb
+++ src/server.rb
@@ -3,13 +3,33 @@ require 'net/scp'
 require 'fileutils'
 
 def srv_init(name, host, password)
+  dist = ""
   Net::SSH.start(host, "root", :password => password) do |ssh|
     ssh.scp.upload!("data/ssh_key.pub", "/tmp/ssh_key.pub")
     ssh.exec "cat /tmp/ssh_key.pub >> /root/.ssh/authorized_keys"
     ssh.exec "rm /tmp/ssh_key.pub"
+    path = ""
+    ssh.exec!("which apt-get")  do |channel, stream, data|
+      path << data if stream == :stdout
+    end
+    if path.include? "/apt-get"
+      dist = "apt"
+    else
+      dist = "yum"
+    end
   end
   FileUtils.mkdir_p("data/servers/" + name)
   f = File.new("data/servers/"+name+"/srv.info",  "w+")
   f.puts host
+  f.puts dist
   f.close
+end
+
+def check_dist
+  file = ssh.exec "which yum"
+  if file.include? "/yum"
+    return "yum"
+  else
+    return "apt"
+  end
 end
