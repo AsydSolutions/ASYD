98080c941ff4c5f198d7aedbee5f14678ca872e5
Choms <choms@botmania.net>
2013-05-23 19:07:50 +0200

    Distro recognition

--- inc/server.rb
+++ inc/server.rb
@@ -3,26 +3,32 @@ require 'net/scp'
 require 'fileutils'
 
 def srv_init(name, host, password)
-  dist = ""
+  distro,dist_name,dist_ver,pkg_mgr = ""
+
   Net::SSH.start(host, "root", :password => password) do |ssh|
     ssh.scp.upload!("data/ssh_key.pub", "/tmp/ssh_key.pub")
     ssh.exec "mkdir -p /root/.ssh && touch /root/.ssh/authorized_keys && cat /tmp/ssh_key.pub >> /root/.ssh/authorized_keys && rm /tmp/ssh_key.pub"
-#    ssh.exec "touch /root/.ssh/authorized_keys"
-#    ssh.exec "cat /tmp/ssh_key.pub >> /root/.ssh/authorized_keys"
-#    ssh.exec "rm /tmp/ssh_key.pub"
-    path = ""
-    ssh.exec!("which apt-get")  do |channel, stream, data|
-      path << data if stream == :stdout
+    ssh.exec!("cat /etc/issue")  do |channel, stream, data|
+      distro << data if stream == :stdout
     end
-    if path.include? "/apt-get"
-      dist = "apt"
-    else
-      dist = "yum"
+
+    distro = distro.split
+    dist_name = distro[0]
+    dist_ver  = distro[2]
+
+    if dist_name == "Debian" or dist_name == "Ubuntu"
+      pkg_mgr = "apt"
+    elsif dist_name == "Fedora" or dist_name == "CentOS"
+      pkg_mgr = "yum"
     end
+
   end
+
   FileUtils.mkdir_p("data/servers/" + name)
   f = File.new("data/servers/"+name+"/srv.info",  "w+")
   f.puts host
-  f.puts dist
+  f.puts dist_name
+  f.puts dist_ver
+  f.puts pkg_mgr
   f.close
 end
