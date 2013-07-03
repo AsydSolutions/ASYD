1ada09b0b83e55cd1496c9d0294219aadcaa9da9
Choms <choms@botmania.net>
2013-07-03 20:10:55 +0200

    New config parser. Helpers for getting host data and ip of the asyd server.

--- inc/helper.rb
+++ inc/helper.rb
@@ -3,6 +3,8 @@ require 'net/ssh'
 require 'net/scp'
 require 'pathname'
 require 'find'
+require 'tempfile'
+require 'socket'
 
 def get_dirs path
   dir_array = Array.new
@@ -18,6 +20,24 @@ def get_files path
     files_array << File.basename(f, "*")
   end
   return files_array
+end
+
+def get_host_data(host)
+  path = "data/servers/"+host+"/srv.info"
+  f = File.open(path, "r")
+  hostdata = {}
+  hostdata[:hostname] = host
+  hostdata[:ip] = f.gets.strip
+  hostdata[:dist_name] = f.gets.strip
+  hostdata[:dist_ver]  = f.gets.strip
+  hostdata[:pkg_mgr] = f.gets.strip
+  f.close
+  return hostdata
+end
+
+def get_asyd_ip
+  ip = UDPSocket.open {|s| s.connect("8.8.8.8", 1); s.addr.last}
+  return ip
 end
 
 def exec_cmd(ip, cmd)
@@ -49,4 +69,30 @@ def download_dir(ip, remote, local)
   Net::SSH.start(ip.strip, "root", :keys => "data/ssh_key") do |ssh|
     ssh.scp.download!(remote, local, :recursive => true)
   end
+end
+
+def parse_config(host, cfg)
+  hostdata = get_host_data(host)
+  hostname = hostdata[:hostname]
+  ip = hostdata[:ip]
+  asyd = get_asyd_ip
+
+  newconf = Tempfile.new('conf')
+  File.open(cfg, "r").each do |line|
+    line = line.split('#')
+    line = line[0]
+    if !line.include?('<%MONITOR:')
+      line.gsub!('<%ASYD%>', asyd)
+      line.gsub!('<%IP%>', ip)
+      line.gsub!('<%HOSTNAME%>', hostname)
+      newconf << line
+    else
+      monitor = line.gsub!(/([<%>])/, '')
+      monitor = monitor.split(':')
+      monitor = monitor[1].strip
+      p monitor
+    end
+  end
+  newconf.flush
+  return newconf
 end
