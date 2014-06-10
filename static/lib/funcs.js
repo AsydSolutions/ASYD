dbe365f39e55b52af3df54896980dd8d14354c2d
Choms <choms@botmania.net>
2014-06-10 16:08:31 +0200

    Asyncronous dismissal of notifications

--- static/lib/funcs.js
+++ static/lib/funcs.js
@@ -44,3 +44,19 @@ function passDataToModal(data, modal_id) {
   $(".modal-footer #dataInput").val( data );
   $(modal_id).modal('show');
 }
+
+function dismissNotification(msg_id)
+{
+  var xmlhttp;
+  if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  }
+  else
+  {// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.open("POST","/notifications/dismiss",true);
+  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); 
+  xmlhttp.send("msg_id=" + msg_id);
+}
