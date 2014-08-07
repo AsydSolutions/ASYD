9f956914fef3467fd833cf5ab4ab4bf7839d9939
Choms <choms@botmania.net>
2014-08-07 12:07:46 +0200

    performance improvements on notifications +minor fixes

--- models/init.rb
+++ models/init.rb
@@ -19,8 +19,9 @@ require_relative "setup"
 require_relative "deploy"
 DataMapper.setup(:tasks_db,  "sqlite3:data/db/tasks.db") #load the tasks database
 require_relative "task"
+DataMapper.setup(:notifications_db,  "sqlite3:data/db/notifications.db") #load the notifications database
 require_relative "notification"
-DataMapper.setup(:monitoring_db,  "sqlite3:data/db/monitoring.db") #load the tasks database
+DataMapper.setup(:monitoring_db,  "sqlite3:data/db/monitoring.db") #load the monitoring database
 require_relative "monitor"
 DataMapper.setup(:hosts_db,  "sqlite3:data/db/hosts.db") #load the hosts database
 require_relative "host"
@@ -28,7 +29,7 @@ require_relative "hostgroup"
 DataMapper.setup(:users_db,  "sqlite3:data/db/users.db") #load the users database
 require_relative "user"
 require_relative "team"
-DataMapper.setup(:status_db, "sqlite3:data/db/status.db")
+DataMapper.setup(:status_db, "sqlite3:data/db/status.db") #load the status database
 require_relative "status"
 DataMapper.finalize
 if File.directory? 'data'
