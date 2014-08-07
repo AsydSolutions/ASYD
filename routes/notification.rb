9eae2595df2c51c49d393cbf577ff67686603583
Choms <choms@botmania.net>
2014-08-07 14:04:48 +0200

    implemented dismiss/acknowledge for monitoring notifications

--- routes/notification.rb
+++ routes/notification.rb
@@ -5,4 +5,18 @@ class ASYD < Sinatra::Application
       notification.update(:dismiss => true)
     end
   end
+
+  post '/notification/monitoring/dismiss' do
+    MNOTEX.synchronize do
+      notification = Monitoring::Notification.first(:id => params['msg_id'].to_i)
+      notification.update(:dismiss => true)
+    end
+  end
+
+  post '/notification/monitoring/acknowledge' do
+    MNOTEX.synchronize do
+      notification = Monitoring::Notification.first(:id => params['msg_id'].to_i)
+      notification.update(:acknowledge => true)
+    end
+  end
 end
