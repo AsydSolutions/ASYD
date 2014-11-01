31e46c74cfcc5c96d9e4d23178ffdd0da10dec4b
Choms <choms@botmania.net>
2014-11-01 17:26:07 +0100

    typo

--- routes/deploys.rb
+++ routes/deploys.rb
@@ -179,7 +179,7 @@ class ASYD < Sinatra::Application
   # accessed by ajax only
   get %r{/deploys/get_file_contents/(.+)} do
     path = params[:captures].first
-    if path.start_with?("data/deploys/") && !path.include? "../"
+    if path.start_with?("data/deploys/") and !path.include? "../"
       send_file path
     end
   end
@@ -188,7 +188,7 @@ class ASYD < Sinatra::Application
   post '/deploys/edit' do
     path = params['path']
     text = params['text']
-    if path.start_with?("data/deploys/") && !path.include? "../"
+    if path.start_with?("data/deploys/") and !path.include? "../"
       open(path, 'w') { |file|
         file.puts text
       }
