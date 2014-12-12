04566ef03d2999daa67d69b9482431aa77733cc1
Daniel Ripoll <info@danielripoll.es>
2014-12-13 00:20:14 +0100

    Error 401,403 and 500 with it's own views.
    If something fails, the user deserve to watch something else than a black&white ugly screen.
    
    --
    Daniel Ripoll <info@danielripoll.es>
    http://danielripoll.es
    +34 6688 27 4 90
    LSYS Network http://lsys.net

--- asyd.rb
+++ asyd.rb
@@ -47,6 +47,21 @@ class ASYD < Sinatra::Application
     erb :oops
   end
 
+  error 401 do
+    status 401
+    erb :error401
+  end
+
+  error 403 do
+    status 403
+    erb :error403
+  end
+
+  error 500 do
+    status 500
+    erb :error500
+  end
+
   # monitoring on the background
   bgmonit = Spork.spork do
    Monitoring.background
