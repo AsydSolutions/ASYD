54c18fec9464bbfc5ec0455a908a4ce95bf50760
Choms <choms@botmania.net>
2015-01-20 15:44:59 +0100

    new free syntax

--- models/misc.rb
+++ models/misc.rb
@@ -62,7 +62,12 @@ module Misc
 
   # Get max allocable forks
   def self.get_max_forks
-    free_mem = %x(free -m |grep cache: |awk '{print $4}')
+    free_version = %x(free -V |awk '{print $4}')
+    if Gem::Version.new(free_version) >= Gem::Version.new('3.3.10')
+      free_mem = %x(free -m |grep Mem: |awk '{print $7}')
+    else
+      free_mem = %x(free -m |grep cache: |awk '{print $4}')
+    end
     max_forks = free_mem.to_i / 30
     return max_forks
   end
