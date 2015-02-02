4acd76e986961848defc7d50c9bdab01e8145a35
Choms <choms@botmania.net>
2015-02-02 17:11:51 +0100

    minor change

--- models/monitor.rb
+++ models/monitor.rb
@@ -105,7 +105,7 @@ module Monitoring
     # 2 == problem
     # 1 == all ok
     def is_ok?
-      if self.opt_vars["monitored"].nil? or self.opt_vars["monitored"].to_i == 0
+      if self.opt_vars["monitored"].nil? or self.opt_vars["monitored"].to_i != 1
         return 4
       else
         hoststatus = nil
