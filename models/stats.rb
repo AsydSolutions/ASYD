7a097c84aaa7cfd0855d0eca8a934d29ec612b02
Choms <choms@botmania.net>
2015-02-26 18:18:44 +0100

    Adding TaskStats model

--- models/stats.rb
+++ models/stats.rb
@@ -7,5 +7,49 @@ class HostStats
 
   property :created_at, DateTime, :key => true
   property :total_hosts, Integer
+end
 
+class TaskStats
+  include DataMapper::Resource
+
+  def self.default_repository_name #here we use the stats_db for the HostStats objects
+    :stats_db
+  end
+
+  property :created_at, DateTime, :key => true
+  property :started_tasks, Integer, :default => 0
+  property :completed_tasks, Integer, :default => 0
+  property :failed_tasks, Integer, :default => 0
+
+  # This function gets a series of date,value and fills the missing dates to complete a month
+  def self.fill_missing_dates(series)
+    series.map do |date, value|
+      [date, value]
+    end.inject([]) do |series, date_and_value|
+      filler = if series.empty?
+        []
+      else
+        ((series.last[0]+ 1)..(date_and_value[0] - 1)).map do |date|
+          [date, 0]
+        end
+      end
+      prefiller = if series.empty?
+        []
+      else
+        (((DateTime.now-30).to_date)..(series.last[0]- 1)).map do |date|
+          [date, 0]
+        end
+      end
+      postfiller = if series.empty?
+        []
+      else
+        ((date_and_value[0] + 1)..((DateTime.now).to_date)).map do |date|
+          [date, 0]
+        end
+      end
+      series + prefiller + filler + [date_and_value] + postfiller
+    end.map do |date, value|
+      [date, value]
+    end
+  end
 end
