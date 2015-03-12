8f4b4c53d991f76c157ed2c35617b2a68d1d5beb
Choms <choms@botmania.net>
2015-03-12 14:12:02 +0100

    deprecated default_cache_size

--- models/init.rb
+++ models/init.rb
@@ -49,42 +49,34 @@ if File.directory? 'data'
   tasks_journal = repository(:tasks_db).adapter.select('PRAGMA journal_mode')[0]
   if tasks_journal != "wal"
     repository(:tasks_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:tasks_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   notifications_journal = repository(:notifications_db).adapter.select('PRAGMA journal_mode')[0]
   if notifications_journal != "wal"
     repository(:notifications_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:notifications_db).adapter.select('PRAGMA default_cache_size = 2000')
   end
   monitoring_journal = repository(:monitoring_db).adapter.select('PRAGMA journal_mode')[0]
   if monitoring_journal != "wal"
     repository(:monitoring_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:monitoring_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   hosts_journal = repository(:hosts_db).adapter.select('PRAGMA journal_mode')[0]
   if hosts_journal != "wal"
     repository(:hosts_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:hosts_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   users_journal = repository(:users_db).adapter.select('PRAGMA journal_mode')[0]
   if users_journal != "wal"
     repository(:users_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:users_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   status_journal = repository(:status_db).adapter.select('PRAGMA journal_mode')[0]
   if status_journal != "wal"
     repository(:status_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:status_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   config_journal = repository(:config_db).adapter.select('PRAGMA journal_mode')[0]
   if config_journal != "wal"
     repository(:config_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:config_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   stats_journal = repository(:stats_db).adapter.select('PRAGMA journal_mode')[0]
   if stats_journal != "wal"
     repository(:stats_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:stats_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
 
   # Set synchronous levels, NORMAL for actively checkpointed and FULL otherwise
