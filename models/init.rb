0035bdedfc7fdbe3c13ff8ab1751bda94281fcf3
Choms <choms@botmania.net>
2015-03-12 14:10:26 +0100

    Set synchronous levels NORMAL for actively checkpointed and FULL otherwise

--- models/init.rb
+++ models/init.rb
@@ -49,51 +49,53 @@ if File.directory? 'data'
   tasks_journal = repository(:tasks_db).adapter.select('PRAGMA journal_mode')[0]
   if tasks_journal != "wal"
     repository(:tasks_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:tasks_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:tasks_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   notifications_journal = repository(:notifications_db).adapter.select('PRAGMA journal_mode')[0]
   if notifications_journal != "wal"
     repository(:notifications_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:notifications_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:notifications_db).adapter.select('PRAGMA default_cache_size = 2000')
   end
   monitoring_journal = repository(:monitoring_db).adapter.select('PRAGMA journal_mode')[0]
   if monitoring_journal != "wal"
     repository(:monitoring_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:monitoring_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:monitoring_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   hosts_journal = repository(:hosts_db).adapter.select('PRAGMA journal_mode')[0]
   if hosts_journal != "wal"
     repository(:hosts_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:hosts_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:hosts_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
   users_journal = repository(:users_db).adapter.select('PRAGMA journal_mode')[0]
   if users_journal != "wal"
     repository(:users_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:users_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:users_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   status_journal = repository(:status_db).adapter.select('PRAGMA journal_mode')[0]
   if status_journal != "wal"
     repository(:status_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:status_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:status_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   config_journal = repository(:config_db).adapter.select('PRAGMA journal_mode')[0]
   if config_journal != "wal"
     repository(:config_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:config_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:config_db).adapter.select('PRAGMA default_cache_size = 500')
   end
   stats_journal = repository(:stats_db).adapter.select('PRAGMA journal_mode')[0]
   if stats_journal != "wal"
     repository(:stats_db).adapter.select('PRAGMA journal_mode = WAL')
-    repository(:stats_db).adapter.select('PRAGMA default_synchronous = 2')
     repository(:stats_db).adapter.select('PRAGMA default_cache_size = 1000')
   end
+
+  # Set synchronous levels, NORMAL for actively checkpointed and FULL otherwise
+  repository(:stats_db).adapter.select('PRAGMA default_synchronous = 2')
+  repository(:config_db).adapter.select('PRAGMA default_synchronous = 2')
+  repository(:users_db).adapter.select('PRAGMA default_synchronous = 2')
+  repository(:tasks_db).adapter.select('PRAGMA default_synchronous = 1')
+  repository(:notifications_db).adapter.select('PRAGMA default_synchronous = 1')
+  repository(:status_db).adapter.select('PRAGMA default_synchronous = 1')
+  repository(:hosts_db).adapter.select('PRAGMA default_synchronous = 1')
+  repository(:monitoring_db).adapter.select('PRAGMA default_synchronous = 1')
 
   # Disable wal autocheckpoint (crashes DB)
   repository(:notifications_db).adapter.select('PRAGMA wal_autocheckpoint = 0')
@@ -114,9 +116,9 @@ if File.directory? 'data'
 
   # Checkpoint at exit to ensure database consistency
   at_exit {
-    repository(:users_db).adapter.select('PRAGMA wal_checkpoint(FULL)')
-    repository(:config_db).adapter.select('PRAGMA wal_checkpoint(FULL)')
-    repository(:stats_db).adapter.select('PRAGMA wal_checkpoint(FULL)')
+    Awal::checkpoint(:users_db)
+    Awal::checkpoint(:config_db)
+    Awal::checkpoint(:stats_db)
   }
 
   if Email.all.first.nil?
