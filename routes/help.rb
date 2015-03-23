c32c9329e6c0ca30ee30f2f34d030b7a00aae316
Choms <choms@botmania.net>
2015-03-23 16:42:45 +0100

    moving documentation from views/ to static/

--- routes/help.rb
+++ routes/help.rb
@@ -7,10 +7,10 @@ class ASYD < Sinatra::Application
   get '/help/:doc' do
     loc = I18n.locale.to_s
     @doc = 'doc/'+loc+'/'+params[:doc]
-    if !File.exists?("views/"+@doc)
+    if !File.exists?("static/"+@doc)
       @doc = 'doc/en/'+params[:doc]
     end
-    if File.exists?("views/"+@doc)
+    if File.exists?("static/"+@doc)
       erb :help
     else
       erb :oops
