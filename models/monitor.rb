aece4f88db5de38c1ac3ee096c0410059c613d38
Choms <choms@botmania.net>
2015-04-28 18:03:41 +0200

    respawn bg monitoring instead of looping

--- models/monitor.rb
+++ models/monitor.rb
@@ -263,31 +263,40 @@ module Monitoring
 
   def self.background
     begin
-      while true
-        max_forks = 15 #we hard limit the max checks to 15 at a time
-        forks = [] #and initialize an empty array
-        hosts = ::Host.all
-        hosts.each do |host|
-          if forks.count >= max_forks #if we reached the max forks
-            forks2 = forks      # Ensure there's no completed forks on the fork list
-            forks2.each do |pid|
-              forks.delete(pid) unless Misc::checkpid(pid)
-            end
-            if forks.count >= max_forks
-              id = Process.wait #then we wait for some child to finish
-              forks.delete(id) #and we remove it from the forks array
-            end
+      max_forks = 15 #we hard limit the max checks to 15 at a time
+      forks = [] #and initialize an empty array
+      hosts = ::Host.all
+      hosts.each do |host|
+        if forks.count >= max_forks #if we reached the max forks
+          forks2 = forks      # Ensure there's no completed forks on the fork list
+          forks2.each do |pid|
+            forks.delete(pid) unless Misc::checkpid(pid)
           end
-          frk = Spork.spork do #so we can continue executing a new fork
-            ret = host.perform_monitoring_operations
-            puts "Error when monitoring "+host.hostname+" on the background: "+ret if ret != true and $DBG == 1
+          if forks.count >= max_forks
+            id = Process.wait #then we wait for some child to finish
+            forks.delete(id) #and we remove it from the forks array
           end
-          forks << frk #and store the fork id on the forks array
         end
-        Process.waitall
-        exit unless Misc::checkpid($ASYD_PID)
-        sleep TTL
+        frk = Spork.spork do #so we can continue executing a new fork
+          ret = host.perform_monitoring_operations
+          puts "Error when monitoring "+host.hostname+" on the background: "+ret if ret != true and $DBG == 1
+        end
+        forks << frk #and store the fork id on the forks array
       end
+      Process.waitall
+      sleep TTL
+      Spork.spork do
+        Process.setsid
+        bgm = Spork.spork do
+          # STDIN.reopen '/dev/null'
+          # STDOUT.reopen '/dev/null', 'a'
+          # STDERR.reopen STDOUT
+          Monitoring::background
+        end
+        $BGMONIT.put_int(0, bgm)
+        exit
+      end
+      exit
     rescue => e
       puts "Error on background monitoring: "+e.message if $DBG == 1
       exit unless Misc::checkpid($ASYD_PID)
