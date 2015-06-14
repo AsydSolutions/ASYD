c4992bfdd044a6c32b446bc37f4b54a363b00d2d
Fusl Dash <fusl@meo.ws>
2015-06-14 18:52:55 +0200

    fix indentations

--- asyd.sh
+++ asyd.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
+
 name="${0}"
 cmd="${1}"
 PIDFile=".asyd.pid"
 
 case ${cmd} in
-  start)
-  unicorn -c unicorn.conf.rb -D
-  echo "ASYD Started"
-  ;;
-  stop)
-  kill -3 $(<"$PIDFile")
-  echo "ASYD Stopped"
-  ;;
-  restart)
-  kill -3 $(<"$PIDFile")
-  sleep 2
-  unicorn -c unicorn.conf.rb -D
-  echo "ASYD Restarted"
-  ;;
-  *)
-  echo "Usage: ${name} {start|stop|restart}"
-  exit 1
-  ;;
+	start)
+		unicorn -c unicorn.conf.rb -D
+		echo "ASYD Started"
+		;;
+	stop)
+		kill -3 $(<"$PIDFile")
+		echo "ASYD Stopped"
+		;;
+	restart)
+		kill -3 $(<"$PIDFile")
+		sleep 2
+		unicorn -c unicorn.conf.rb -D
+		echo "ASYD Restarted"
+		;;
+	*)
+		echo "Usage: ${name} {start|stop|restart}"
+		exit 1
+		;;
 esac
