d03930c63d77f787c57776863f2ee54e2514933f
Choms <choms@botmania.net>
2015-07-05 22:21:07 +0200

    new daemon handler + reconnect DB on fork

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,6 +1,6 @@
 $UNICORN = 1
 $ASYD_PID = Process.pid
-$ASYD_VERSION = 0.0901
+$ASYD_VERSION = 0.091
 $DBG = 0 #debug?
 
 FileUtils.mkdir("log") unless File.directory?("log")
@@ -16,15 +16,13 @@ GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
+  if File.exist?('data/.dmon.pid')
+    Dmon::stop('dmon')
+  end
+
   DataObjects::Pooling.pools.each do |pool|
     pool.dispose
   end
-
-  wck = $WALCHECK.get_int(0)
-  bgm = $BGMONIT.get_int(0)
-
-  Process.kill("TERM", wck) if wck > 0
-  Process.kill("TERM", bgm) if bgm > 0
 end
 
 after_fork do |server, worker|
@@ -37,29 +35,5 @@ after_fork do |server, worker|
   DataMapper.setup(:config_db, "sqlite3:data/db/config.db") #load the config database
   DataMapper.setup(:stats_db, "sqlite3:data/db/stats.db") #load the stats database
 
-  # check for checkpoints
-  Spork.spork do
-    Process.setsid
-    wck = Spork.spork do
-      # STDIN.reopen '/dev/null'
-      # STDOUT.reopen '/dev/null', 'a'
-      # STDERR.reopen STDOUT
-      Awal::should_checkpoint?
-    end
-    $WALCHECK.put_int(0, wck)
-    exit
-  end
-
-  # monitoring on the background
-  Spork.spork do
-    Process.setsid
-    bgm = Spork.spork do
-      # STDIN.reopen '/dev/null'
-      # STDOUT.reopen '/dev/null', 'a'
-      # STDERR.reopen STDOUT
-      Monitoring.background
-    end
-    $BGMONIT.put_int(0, bgm)
-    exit
-  end
+  Dmon::init
 end
