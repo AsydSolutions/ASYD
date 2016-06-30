cf1ea5bcaf136e51963779a4888dd1ca19c31dfb
Choms <sergio@asyd-solutions.com>
2016-06-30 14:02:37 +0200

    Make groups overview distinguish critical/warning server status (solves #119)

--- models/monitor.rb
+++ models/monitor.rb
@@ -261,18 +261,21 @@ module Monitoring
     def members_status
       stat = {}
       stat[:total] = self.hosts.count
-      stat[:failed] = 0
+      stat[:warning] = 0
+      stat[:critical] = 0
       stat[:sane] = 0
       if stat[:total] == 0
         return stat
       else
         self.hosts.each do |host|
           ret = host.is_ok?
-          if ret != 1
-            stat[:failed] = stat[:failed] + 1
+          if ret == 2
+            stat[:warning] = stat[:warning] + 1
+          elsif ret == 3
+            stat[:critical] = stat[:critical] + 1
           end
         end
-        stat[:sane] = stat[:total] - stat[:failed]
+        stat[:sane] = stat[:total] - stat[:warning] - stat[:critical]
         return stat
       end
     end
